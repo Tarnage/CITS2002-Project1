9f3ccfb0872f2a2a93a83b1740500c4d05a576a5
Tom <1991tnguyen@gmail.com>
2021-08-24 11:30:16 +0800

    init

---
+++ Helper Functions/decimalToHexa.c
@@ -0,0 +1,19 @@
+// This function will convert decimal to hexa and return the hexa value 
+#include <stdio.h>
+
+
+
+
+char decimalToHexa(uint16_t decimal){
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+    return 0
+}
